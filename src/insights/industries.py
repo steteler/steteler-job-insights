e1fb891a58f1cea3878a383913ed14847c8139b9
kikuti <joaovictorkikuti@gmail.com>
2023-04-03 22:17:53 -0300

    feat: req 3, implements get_unique_industries function to return unique industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,9 @@
-from typing import List, Dict
+from typing import List, Dict, Set
+from src.insights.jobs import read
 
 
-def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+def get_unique_industries(path: str) -> Set[str]:
+    return set(row["industry"] for row in read(path) if len(row["industry"]))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
