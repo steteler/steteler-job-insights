078fcdfb2b75834a0a2e1ba3977e9d34ea641efc
kikuti <joaovictorkikuti@gmail.com>
2023-04-04 01:05:56 -0300

    feat: req 7, implements filter_by_industry function to return jobs industry equal to industry paramater

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,18 +7,4 @@ def get_unique_industries(path: str) -> Set[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return list(row for row in jobs if row["industry"] == industry)
