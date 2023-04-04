2ab8446b2f515b6ebc8c371a1818ad589d24fe63
kikuti <joaovictorkikuti@gmail.com>
2023-04-04 01:03:43 -0300

    refactor: req6, removed comments

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -14,18 +14,4 @@ def get_unique_job_types(path: str) -> Set[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     return list(row for row in jobs if row["job_type"] == job_type)
