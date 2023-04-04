5c573c81daa9ad4b819d6713e5e062b6efaa5c89
kikuti <joaovictorkikuti@gmail.com>
2023-04-04 04:25:01 -0300

    feat: req 10, implements test_counter function to test count_ocurrences

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,36 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+jobs_fixture = [
+    {
+        "min_salary": 10,
+        "max_salary": 20,
+        "date_posted": "2023-01-02",
+        "job_title": "Back End Developer",
+    },
+    {
+        "min_salary": 1,
+        "max_salary": 25,
+        "date_posted": "2022-01-02",
+        "job_title": "Front End Developer",
+    },
+    {
+        "min_salary": 150,
+        "max_salary": 200,
+        "date_posted": "2021-01-02",
+        "job_title": "Full Stack Developer",
+    },
+]
+
+
+@pytest.fixture
+def jobs():
+    return jobs_fixture
+
+
+def test_sort_by_criteria(jobs):
+    job_list = jobs
+    sort_by(job_list, "max_salary")
+    assert job_list[0]["job_title"] == "Full Stack Developer"
+    assert job_list[2]["job_title"] == "Back End Developer"
